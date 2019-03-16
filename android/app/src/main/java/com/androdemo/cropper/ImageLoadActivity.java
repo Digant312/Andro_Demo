3bf511425300f42cffefadc15e1f82fbcbf827cb
Harsh Patel <harsh.patel@ihsmarkit.com>
2019-03-17 00:18:06 +0530

    - Integrated cropping UI to application.

--- android/app/src/main/java/com/androdemo/cropper/ImageLoadActivity.java
+++ android/app/src/main/java/com/androdemo/cropper/ImageLoadActivity.java
@@ -3,10 +3,13 @@ package com.androdemo.cropper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.androdemo.R;
 import com.androdemo.constants.Constant;
@@ -17,9 +20,10 @@ import com.facebook.react.ReactActivity;
 public class ImageLoadActivity extends ReactActivity
 {
 
-    Button btnSelectImage;
-    String imagePath="";
-    ImageView profilePicture;
+    private String imagePath = "";
+    private ImageView profilePicture;
+    private AppCompatSeekBar seekZoomController;
+    private CardView btnCancel,btnDone;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -31,38 +35,75 @@ public class ImageLoadActivity extends ReactActivity
         getImagePath();
     }
 
-    private void getImagePath(){
-        if(getIntent().hasExtra(Constant.kIMAGE_PATH)){
-            imagePath = getIntent().getStringExtra(Constant.kIMAGE_PATH);
-
-            if(imagePath!=null){
-                Log.v("Verbose","Setting path : "+imagePath);
-
-                Glide.with(this)
-                     .load(imagePath)
-                     .into(profilePicture);
-            }else{
-                Log.v("Error","Image path is null");
-            }
-        }
-
-    }
-
     private void init()
     {
-        btnSelectImage = findViewById(R.id.btnSelectImage);
         profilePicture = findViewById(R.id.profilePicture);
+        seekZoomController = findViewById(R.id.seekZoomController);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnDone = findViewById(R.id.btnDone);
 
-        btnSelectImage.setOnClickListener(new View.OnClickListener()
+        btnCancel.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(ImageLoadActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDone.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
                 Intent intent = new Intent();
-                intent.putExtra(Constant.kKEY, "Path to cropped image");
-                setResult(RESULT_OK,intent);
+                intent.putExtra(Constant.kKEY, imagePath);
+                setResult(RESULT_OK, intent);
                 finish();
             }
         });
+        seekZoomController.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                float scale = ((progress / 10.0f) + 1);
+                profilePicture.setScaleX(scale);
+                profilePicture.setScaleY(scale);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
+    }
+
+    private void getImagePath()
+    {
+        if (getIntent().hasExtra(Constant.kIMAGE_PATH))
+        {
+            imagePath = getIntent().getStringExtra(Constant.kIMAGE_PATH);
+
+            if (imagePath != null)
+            {
+                Log.v("Verbose", "Setting path : " + imagePath);
+
+                Glide.with(this)
+                     .load(imagePath)
+                     .into(profilePicture);
+            }
+            else
+            {
+                Log.v("Error", "Image path is null");
+            }
+        }
     }
 }
